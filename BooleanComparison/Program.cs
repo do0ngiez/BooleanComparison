fab1d4d38b58e5bb7f984d54586476aeb8803f24
Kriszha Real <yujmin901205@gmail.com>
2024-10-02 05:47:27 +0800

    Add project files.

---
+++ BooleanComparison/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            bool condition1 = true;
+            while (condition1)
+            {
+                Console.WriteLine("This is a while loop.");
+                condition1 = false;
+            }
+
+            //do-while loop
+            bool condition2 = false;
+            do
+            {
+                Console.WriteLine("This is a do-while loop.");
+            } while (condition2);
+        }
+    }
+}
